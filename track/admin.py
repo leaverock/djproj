30aa822eefb1ca691547b5ec42bf43b5b92ea296
leaverock <35506776+leaverock@users.noreply.github.com>
2018-05-20 18:33:42 +0300

    Удаление лишней регистрации в интерфейсе админа.

--- track/admin.py
+++ track/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from track.models import UchSTRUCT, StansSTRUCT, CategSTRUCT
 
-admin.site.register(UchSTRUCT)
-admin.site.register(StansSTRUCT)
-admin.site.register(CategSTRUCT)
+# Register your models here.
--- vars_results/admin.py
+++ vars_results/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from vars_results.models import TjnHDstruct, TJNtyp, TJrSTRUCT, RegcSTRUCT
 
-admin.site.register(TjnHDstruct)
-admin.site.register(TJNtyp)
-admin.site.register(TJrSTRUCT)
-admin.site.register(RegcSTRUCT)
+# Register your models here.
