8bc3e6e086430a9c2e5ce124edb60e65016f275c
yrden <yaroschuk.denis@vniizht.ru>
2019-01-28 10:05:55 +0300

    Revert "VEIP запущен из браузера"
    удаление упоминания веипа

--- veip/views.py
+++ veip/views.py
@@ -1,20 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .run.jan25_19.veip_21jan19.init__ import go
 
 
 def index(request):
     context = {}
     return render(request, 'veip\\index.html', context)
 
-
 def vivod(request):
     sp = request.POST['speed']
     rad = request.POST['radius']
-
-    go(9, 16, 1, 1, 0, 1, float(sp), float(rad), 0.8, 0.05, 556, 1900.0, 0.007, 1.0, 0.8)
-
-    with open(r'C:\work\veip\djproj\veip\run\vivod', 'r') as file:
+    with open(r'C:\Users\yaroschuk.denis\Desktop\ФОРП СЕНДЕРКИН.txt', 'r') as file:
         text = file.read()
 
-    return HttpResponse(f"<pre>{text}</pre>")
+    return HttpResponse(text)#"скорость: %.3f is %.3f." % (float(sp), float(rad)))
